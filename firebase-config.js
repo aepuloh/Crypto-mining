42bc002b536d7b35eaabd0693e221964cfefe33c
aepuloh <57800632+aepuloh@users.noreply.github.com>
2025-08-17 21:04:37 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// ==== GANTI DENGAN PUNYAMU====
+export const firebaseConfig = {
+  apiKey: "AIzaSyCc4ix1uGCaE-rsyM6Lg3jo6SzVjbXYCmw",
+  authDomain: "crypto-mining-d3811.firebaseapp.com",
+  projectId: "crypto-mining-d3811",
+  storageBucket: "crypto-mining-d3811.firebasestorage.app",
+  messagingSenderId: "1068882455445",
+  appId: "1:1068882455445:web:362538bf3bb36c598f649c"
+};
+
+// UID admin yang diizinkan buka admin.html (boleh lebih dari 1)
+export const ADMIN_UIDS = [
+  "UtDD46hVtkNckFSep1TNqqE1oaa2",
+  // "saVj1QxN29cXRRq4Sr5pJ0utRB42",
+];
